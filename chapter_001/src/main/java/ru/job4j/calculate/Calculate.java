598e4bc63afcc9e695bdb1ae3621e74762c89ddb
TimurCheshuin <cation92@yandex.ru>
2019-05-30 21:42:30 +0700

    1.3. JavaDoc. Code convention[#144598]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,3 +1,13 @@
+/*
+    Class Calculate includes the Method main
+    Method main - the point of entry into program
+    @params String[] args - array of strings
+    @return output string to console
+    @author Timur Cheshuin
+    @since 28.05.19
+    @version 1
+*/
+
 package ru.job4j.calculate;
 
 public class Calculate {
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Timur Cheshuin (cation92@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
