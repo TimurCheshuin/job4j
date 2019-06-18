2cbed37bbeffc7652455fd0530c35a54f7a2e09e
TimurCheshuin <cation92@yandex.ru>
2019-06-19 00:08:29 +0700

    2. Чтение кода и базовые ошибки.[#144644]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,25 +1,21 @@
 package ru.job4j.loop;
-
 /**
  * The Class for calculating the factorial.
  * @author Timur Cheshuin
  * @since 08.06.19
  * @version 1
  */
-
 public class Factorial {
-
     /**
      * Method calc.
-     * @param n positive integer.
+     * @param number positive integer.
      * @return the factorial of a number n.
      */
-
-    public int calc(int n) {
+    public int calculate(int number) {
         int result = 1;
-        if (n > 0) {
-            for (int i = 1; i <= n; i++) {
-                result *= i;
+        if (number > 0) {
+            for (int index = 1; index <= number; index++) {
+                result *= index;
             }
         }
         return result;
--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -1,38 +1,31 @@
 package ru.job4j.loop;
-
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
 /**
  * Test
  * @author Timur Cheshuin
  * @since 08.06.19
  * @version 1
  */
-
 public class FactorialTest {
-
     /**
      * Test calc.
      */
-
     @Test
     public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
         Factorial fact = new Factorial();
-        int result = fact.calc(5);
+        int result = fact.calculate(5);
         assertThat(result, is(120));
     }
-
     /**
      * Test calc.
      */
-
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
         Factorial fact = new Factorial();
-        int result = fact.calc(0);
+        int result = fact.calculate(0);
         assertThat(result, is(1));
     }
 }
